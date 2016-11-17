4832c65ef251a364b79330dcdd78da462b72bf23
novirael <novirael@gmail.com>
2016-11-17 22:59:14 +0100

    Dodanie możliwości wylogowania z systemu

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url('^register/', views.RegisterView.as_view(), name='register'),
     url('^login/', views.LoginView.as_view(), name='login'),
+    url('^logout/', auth_views.logout, name='logout')
 ]
