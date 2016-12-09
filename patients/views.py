b50dcd2b1b1c8b99014c06e18a7fa4193dc68242
novirael <novirael@gmail.com>
2016-12-09 22:32:33 +0100

    Cofnięcie podpisu dla detali pacjenta

--- patients/views.py
+++ patients/views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, TemplateView
 
 from patients.models import Relationship
@@ -19,22 +17,6 @@ class PatientIndex(ListView):
 class PatientDetails(TemplateView):
     template_name = 'patients/details.html'
     patient = None
-
-    def dispatch(self, request, *args, **kwargs):
-        if self.request.session.get('is_authenticated') is not True:
-            url = reverse('accounts:authenticate')
-
-            redirect_url = reverse('patients:details', kwargs={'pk': kwargs['pk']})
-
-            full_redirect_url = '{base_url}?{redirect_field_name}={redirect_url}'.format(
-                base_url=url,
-                redirect_field_name=REDIRECT_FIELD_NAME,
-                redirect_url=redirect_url
-            )
-
-            return redirect(full_redirect_url)
-
-        return super(PatientDetails, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         patient = get_object_or_404(
