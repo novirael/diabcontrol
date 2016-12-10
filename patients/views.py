7cab40f58ed430ac840caff12347ff3a413b427a
Łukasz Kanikuła <lukasz.kanikula.fm@gmail.com>
2016-12-10 17:59:24 +0100

    Sekcja makroskladnikow i wysilku - widok dzienny #32 #33

--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import get_object_or_404
@@ -70,10 +70,97 @@ class DailyResultsDetails(PatientDateMixin, TemplateView):
         return context
 
     def get_activity_context(self):
-        return {}
+        steps_data = ReportData.objects.filter(
+            type='steps',
+            patient_id=self.patient.id,
+            datetime__day=self.current_date.day,
+            datetime__month=self.current_date.month,
+            datetime__year=self.current_date.year,
+        )
+
+        climbed_data = ReportData.objects.filter(
+            type='flights_climbed',
+            patient_id=self.patient.id,
+            datetime__day=self.current_date.day,
+            datetime__month=self.current_date.month,
+            datetime__year=self.current_date.year,
+        )
+
+        heartrate_data_act = ReportData.objects.filter(
+            type='heart_rate',
+            patient_id=self.patient.id,
+            datetime__day=self.current_date.day,
+            datetime__month=self.current_date.month,
+            datetime__year=self.current_date.year,
+        )
+
+        heartrate_data_avg = ReportData.objects.filter(
+            type='heart_rate',
+            patient_id=self.patient.id,
+            datetime__month=(self.current_date.replace(day=1) - timedelta(days=1)).month,
+        )
+
+        if heartrate_data_avg:
+            avg_prev_hr = sum(
+                [data.value for data in heartrate_data_avg]
+            )/len(heartrate_data_avg)
+        else:
+            avg_prev_hr = 0
+
+        if heartrate_data_act:
+            actual_avg_heartrate = sum(
+                [data.value for data in heartrate_data_act]
+            )
+        else:
+            actual_avg_heartrate = 0
+
+        hr_status = 'default'
+
+        if avg_prev_hr * 1.3 >= actual_avg_heartrate \
+                or avg_prev_hr * 0.7 <= actual_avg_heartrate:
+            hr_status = 'warning'
+        if avg_prev_hr * 1.5 >= actual_avg_heartrate \
+                or avg_prev_hr * 0.5 <= actual_avg_heartrate:
+            hr_status = 'danger'
+
+        return {
+            'steps': sum([data.value for data in steps_data]),
+            'flights_climbed': sum([data.value for data in climbed_data]),
+            'heart_rate': sum([data.value for data in heartrate_data_act]),
+            'avg_prev_hr': avg_prev_hr,
+            'hr_status': hr_status if avg_prev_hr else 'default'
+        }
 
     def get_nutrition_context(self):
-        return {}
+        fat_data = ReportData.objects.filter(
+            type='fat',
+            patient_id=self.patient.id,
+            datetime__day=self.current_date.day,
+            datetime__month=self.current_date.month,
+            datetime__year=self.current_date.year,
+        )
+
+        protein_data = ReportData.objects.filter(
+            type='protein',
+            patient_id=self.patient.id,
+            datetime__day=self.current_date.day,
+            datetime__month=self.current_date.month,
+            datetime__year=self.current_date.year,
+        )
+
+        carb_data = ReportData.objects.filter(
+            type='carbohydrates',
+            patient_id=self.patient.id,
+            datetime__day=self.current_date.day,
+            datetime__month=self.current_date.month,
+            datetime__year=self.current_date.year,
+        )
+
+        return {
+            'fat': sum([data.value for data in fat_data]),
+            'protein': sum([data.value for data in protein_data]),
+            'carbohydrates': sum([data.value for data in carb_data])
+            }
 
     def get_glucose_context(self):
         glucose_data = ReportData.glucose_measurement.filter(
